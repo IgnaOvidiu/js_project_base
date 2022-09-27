a29a1ef033285e7ae39094e6b045c0582f58b00f
IgnaOvidiu <108483395+IgnaOvidiu@users.noreply.github.com>
2022-09-27 17:18:52 +0300

    fetch and dom output

--- src/js/search.js
+++ src/js/search.js
@@ -6,6 +6,33 @@ const searchBtn = document.querySelector('#button-addon2');
 searchBtn.addEventListener('click', getRecipeList);
 
 function getRecipeList() {
-  let searchInputText = document.querySelector('#search').value;
-  fetch(`${API_URL}/filter.php?i=${searchInputText}`);
+  let searchInputTxt = document.getElementById('search').value.trim();
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let html = '';
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          html += `
+            <div class = "recipe" data-id = "${meal.idMeal}">
+                <div class="card" style="width: 18rem;">
+                  <img src = "${meal.strMealThumb}" alt = "food" class="card-img-top">
+                  <div class="card-body text-center">
+                    <h3 class="fs-6">${meal.strMeal}</h3>
+                    <a href = "#" class = "btn btn-primary recipe-btn">Get Recipe</a>
+                  </div>
+                  <button type="button" class="btn btn-primary" id="save"> Save For Later </button>
+                </div>
+              </div>
+            `;
+        });
+        recipeList.classList.remove('notFound');
+      } else {
+        html = "Sorry, we didn't find any meal!";
+        recipeList.classList.add('notFound');
+      }
+      recipeList.innerHTML = html;
+    });
 }
