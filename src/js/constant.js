473c90c47b85eeffa70137cc764e0d7aad9bd645
IgnaOvidiu <108483395+IgnaOvidiu@users.noreply.github.com>
2022-10-03 18:31:20 +0300

    .

--- src/js/constant.js
+++ src/js/constant.js
@@ -1,4 +1,5 @@
 export const API_URL = 'https://api.edamam.com/api/recipes/v2';
+export const API_KEY = '3cb67c31ca45be3a00e386fd5d1406d1';
 export const APP_ID = 'b0e61b12';
 
 export const recipeList = document.querySelector('#recipe');
